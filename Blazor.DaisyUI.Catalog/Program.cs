4bb0320dd989fa85773a984cab8e45f08232991f
Denny09310 <k.denny2000@hotmail.com>
2023-12-27 11:22:12 +0100

    feat(carousel): added navigation buttons

--- Blazor.DaisyUI.Catalog/Program.cs
+++ Blazor.DaisyUI.Catalog/Program.cs
@@ -1,10 +1,13 @@
 using Blazor.DaisyUI.Catalog.Components;
+using Blazor.DaisyUI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddDaisyUi();
 
 var app = builder.Build();
 
